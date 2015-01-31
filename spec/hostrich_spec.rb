8c9c04c1a5e633125a1f06a0eeb6dfdf5e77b6b4
Rafaël Blais Masson <rafbmasson@gmail.com>
2015-01-31 12:02:34 -0500

    Improve specs to cover multiple hosts

--- spec/hostrich_spec.rb
+++ spec/hostrich_spec.rb
@@ -10,53 +10,60 @@ describe Hostrich do
       when '/redirect'
         [302, { 'Location' => 'http://foo.com/index.html' }, []]
       when '/cookie'
-        [200, { 'Set-Cookie' => 'some_param=foo.com/index.html; Path=/; Domain=.foo.com' }, ['Cookie!']]
+        [200, { 'Set-Cookie' => 'some_param=foo.com/index.html; Path=/; Domain=.foo.bar.io' }, ['Cookie!']]
       when '/version.rb'
         Rack::File.new('lib/hostrich').call(env)
       else
-        [200, {}, ['Welcome to foo.com or foo.com.dev, not foo.comzle, not ffoo.com and not bar.io']]
+        [200, {}, ['Welcome to foo.com, foo.com.dev or foo.bar.io, not foo.comzle, not ffoo.com and not bar.io']]
       end
     }
   }
   let(:request) { Rack::MockRequest.new(stack) }
 
-  shared_examples 'the whole deal' do
-    it 'adds host suffix in response body' do
-      response = request.get('/', 'HTTP_HOST' => 'foo.com.dev')
-      expect(response.body).to eq 'Welcome to foo.com.dev or foo.com.dev, not foo.comzle, not ffoo.com and not bar.io'
-    end
-
+  shared_examples 'adds suffix to single host' do
     it 'adds host suffix in response headers' do
       response = request.get('/redirect', 'HTTP_HOST' => 'foo.com.dev')
       expect(response.headers['Location']).to eq 'http://foo.com.dev/index.html'
 
       response = request.get('/cookie', 'HTTP_HOST' => 'foo.com.dev')
-      expect(response.headers['Set-Cookie']).to eq 'some_param=foo.com.dev/index.html; Path=/; Domain=.foo.com.dev'
+      expect(response.headers['Set-Cookie']).to eq 'some_param=foo.com.dev/index.html; Path=/; Domain=.foo.bar.io'
     end
 
     it 'works with Rack::File' do
       response = request.get('/version.rb', 'HTTP_HOST' => 'foo.com.dev')
       expect(response.body).to include "class Hostrich\n  VERSION"
     end
+
+    it 'adds host suffix in response body' do
+      response = request.get('/', 'HTTP_HOST' => 'foo.com.dev')
+      expect(response.body).to eq 'Welcome to foo.com.dev, foo.com.dev or foo.bar.io, not foo.comzle, not ffoo.com and not bar.io'
+    end
   end
 
   context 'passing host as a string' do
     let(:stack) { Hostrich.new(app, 'foo.com') }
-    it_behaves_like 'the whole deal'
+    include_examples 'adds suffix to single host'
   end
 
   context 'passing host as an array' do
     let(:stack) { Hostrich.new(app, ['foo.com']) }
-    it_behaves_like 'the whole deal'
+    include_examples 'adds suffix to single host'
   end
 
   context 'passing multiple hosts as an array' do
     let(:stack) { Hostrich.new(app, %w[foo.com foo.bar.io]) }
-    it_behaves_like 'the whole deal'
 
-    it 'adds host suffix for all hosts' do
+    it 'adds host suffix to multiple hosts in response headers' do
+      response = request.get('/redirect', 'HTTP_HOST' => 'foo.com.dev')
+      expect(response.headers['Location']).to eq 'http://foo.com.dev/index.html'
+
+      response = request.get('/cookie', 'HTTP_HOST' => 'foo.com.dev')
+      expect(response.headers['Set-Cookie']).to eq 'some_param=foo.com.dev/index.html; Path=/; Domain=.foo.bar.io.dev'
+    end
+
+    it 'adds host suffix to multiple hosts in response body' do
       response = request.get('/', 'HTTP_HOST' => 'foo.com.127.0.0.1.xip.io')
-      expect(response.body).to eq 'Welcome to foo.com.127.0.0.1.xip.io or foo.com.127.0.0.1.xip.io.dev, not foo.comzle, not ffoo.com and not bar.io'
+      expect(response.body).to eq 'Welcome to foo.com.127.0.0.1.xip.io, foo.com.127.0.0.1.xip.io.dev or foo.bar.io.127.0.0.1.xip.io, not foo.comzle, not ffoo.com and not bar.io'
     end
   end
 
@@ -65,12 +72,12 @@ describe Hostrich do
 
     it 'does nothing' do
       response = request.get('/', 'HTTP_HOST' => 'foo.com.dev')
-      expect(response.body).to eq 'Welcome to foo.com or foo.com.dev, not foo.comzle, not ffoo.com and not bar.io'
+      expect(response.body).to eq 'Welcome to foo.com, foo.com.dev or foo.bar.io, not foo.comzle, not ffoo.com and not bar.io'
     end
 
     context 'adding hosts after initialization' do
       before { Hostrich.hosts += ['foo.com'] }
-      it_behaves_like 'the whole deal'
+      include_examples 'adds suffix to single host'
     end
   end
 end
